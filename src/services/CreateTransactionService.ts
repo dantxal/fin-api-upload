794fc1de0c69032822e0abd1bddf2c08e1d2dcab
dantxal <dantxal@gmail.com>
2020-09-19 13:09:13 -0300

    Basic create transaction and get balance

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,62 @@
+import { getCustomRepository } from 'typeorm';
 // import AppError from '../errors/AppError';
 
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    type,
+    value,
+    category,
+  }: CreateTransactionDTO): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
+    // const categoryRepository = getRepository(Category);
+
+    // let existingCategory = await categoryRepository.findOne({
+    //   where: {
+    //     title: category,
+    //   },
+    // });
+
+    // if (!existingCategory) {
+    //   const newCategory = await categoryRepository.create({
+    //     title: category,
+    //   });
+    //   existingCategory = await categoryRepository.save(newCategory);
+    // }
+
+    // const transaction = await transactionsRepository.create({
+    //   title,
+    //   type,
+    //   value,
+    //   category_id: existingCategory.id,
+    // });
+
+    // await transactionsRepository.save(transaction);
+
+    // console.log(existingCategory, transaction);
+
+    // return transaction;
   }
 }
 
